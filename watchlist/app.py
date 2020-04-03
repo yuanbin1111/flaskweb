00d6882f33a8d0a060c096ea0b7c7cb626ae1bf1
yuanbin1111 <1289340604@qq.com>
2020-04-03 22:02:45 +0800

    数据库添加内容

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,10 +1,34 @@
+import os,sys
+
 from flask import Flask,url_for,render_template
 from flask_sqlalchemy import SQLAlchemy
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    perfix = 'sqlite:///'   #window平台
+else:
+    perfix = 'sqlite:////'  #Mac Linux平台
 
 app = Flask(__name__)
 
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = perfix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+
+#view 视图函数
 @app.route('/')
 def index():
     name = "元斌"
@@ -19,3 +43,12 @@ def index():
         {"title":"疯狂外星人","year":"2018"},
     ]
     return render_template("index.html",name=name,movies=movies)
+
+#自定义命令
+@app.cli.command()   #把以下内容注册为命令
+@click.option('--drop',is_flag=True,help="先删除后创建")  
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化成功")
