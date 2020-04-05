dc8ff6b45dd20c638fedc004f1e764f99ed920e5
yuanbin1111 <1289340604@qq.com>
2020-04-05 17:12:44 +0800

    表单操作

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,6 +1,6 @@
 import os,sys
 
-from flask import Flask,url_for,render_template
+from flask import Flask,url_for,render_template,request,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 import click
 
@@ -14,6 +14,8 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = perfix + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '1903_dev'
+
 db = SQLAlchemy(app)
 
 
@@ -29,8 +31,22 @@ class Movie(db.Model):
 
 
 #view 视图函数
-@app.route('/')
+@app.route('/',methods=["POST","GET"])
 def index():
+    if request.method == "POST":
+        #获取表单数据
+        title = request.form.get('title')
+        year = request.form.get('year')
+        #验证数据
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash("输入错误")
+            return redirect(url_for('index'))
+        #如果条件都满足，就将数据保存到数据库
+        movie = Movie(title=title,year=year)  #创建记录
+        db.session.add(movie)
+        db.session.commit()
+        flash("添加数据成功")
+        return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template("index.html",movies=movies)
 
