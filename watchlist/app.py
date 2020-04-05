4aa4aa4e1610b105b4e6df8b61b1e92394cc5644
yuanbin1111 <1289340604@qq.com>
2020-04-05 16:18:16 +0800

    模板上下文/继承

--- watchlist/app.py
+++ watchlist/app.py
@@ -31,18 +31,8 @@ class Movie(db.Model):
 #view 视图函数
 @app.route('/')
 def index():
-    name = "元斌"
-    movies = [
-        {"title":"大赢家","year":"2020"},
-        {"title":"战狼","year":"2015"},
-        {"title":"战狼2","year":"2018"},
-        {"title":"囧妈","year":"2020"},
-        {"title":"叶问","year":"2019"},
-        {"title":"杀破狼","year":"2017"},
-        {"title":"极限特工","year":"2016"},
-        {"title":"疯狂外星人","year":"2018"},
-    ]
-    return render_template("index.html",name=name,movies=movies)
+    movies = Movie.query.all()
+    return render_template("index.html",movies=movies)
 
 #自定义命令
 @app.cli.command()   #把以下内容注册为命令
@@ -57,5 +47,11 @@ def initdb(drop):
 #自定义错误页面
 @app.errorhandler(404)
 def page_not(a):
-    user = User.query.first()
-    return render_template('404.html',user=user)
+    return render_template('404.html')
+
+
+#模板上下文处理函数
+@app.context_processor
+def common_user():
+    user =User.query.first()
+    return dict(user=user)
