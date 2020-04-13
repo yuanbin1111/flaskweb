041513fc01de4f7ab22a05ac3ed249e6e93e8b04
yuanbin1111 <1289340604@qq.com>
2020-04-14 00:38:23 +0800

    vue运算

--- practise/src/App.vue
+++ practise/src/App.vue
@@ -1,62 +1,66 @@
 <template>
-  <div class="con" id="app">
-    <h2>shooping list</h2>
-    <div class="inp_shop">
-      <input class="js-new" type="text" placeholder="you want to buy" />
-      <span class="btu">
-        <button class="js-add btn btn-default">add shopping</button>
-      </span>
-    </div>
-    <ul>
-      <li>
-        <div class="checkbox">
-          <label for>
-            <input class="js-item" type="checkbox" />apple
-          </label>
-        </div>
-      </li>
-      <li>
-        <div class="checkbox">
-          <label for>
-            <input class="js-item" type="checkbox" />banana
-          </label>
-        </div>
-      </li>
-      <li>
-        <div class="checkbox">
-          <label for>
-            <input class="js-item" type="checkbox" />organ
-          </label>
-        </div>
-      </li>
-    </ul>
-    <div class="footer">
-      <span>change title</span>
-      <input type="text" class="js-change" placeholder="shopping list" />
-    </div>
+<div class="container">
+  <h2>我的购物车清单</h2>
+  <div class="input-group">
+    <input class="js-new-item form-control" type="text" placeholder="输入你想购买的物品" />
+    <span class="input-group-btn">
+      <button type="button" class="js-add btn btn-default">添加到购物车</button>
+    </span>
   </div>
+  <!-- 购物车列表 -->
+  <ul>
+    <li>
+      <div class="checkbox">
+        <label for>
+          <input type="checkbox" class="js-item" /> 苹果
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="checkbox">
+        <label for>
+          <input type="checkbox" class="js-item" /> 香蕉
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="removed">
+        <label for>
+          <input type="checkbox" class="js-item" /> 电脑
+        </label>
+      </div>
+    </li>
+  </ul>
+  <!-- 修改标题 -->
+  <div class="footer">
+    <hr />
+    <span>改变你的标题</span>
+    <input type="text" class="js-change-title" value="我的购物车清单" />
+  </div>
+</div>
 </template>
 
 <script>
 </script>
 
 <style>
-.con {
+.container {
   width: 40%;
   margin: 100px auto;
 }
-ul {
-  border: 1px solid black;
-  margin-top: 20px;
-}
-ul li {
-  list-style-type: none;
-}
+
 .removed {
   color: gray;
 }
+
 .removed label {
   text-decoration: line-through;
 }
+ul{
+  border: 1px solid black;
+  margin-top: 30px;
+}
+ul li{
+  list-style: none;
+}
 </style>
-
---
+++ vuePlugin/VueMathPlugin.js
@@ -0,0 +1,20 @@
+export default {
+    install: function (Vue) {
+        // 自定义指令 square  v-square
+        Vue.directive('square', function (el, binding) {
+            el.innerHTML = Math.pow(binding.value, 2)
+        });
+        Vue.directive('sqrt', function (el, binding) {
+            el.innerHTML = Math.sqrt(binding.value)
+        });
+        Vue.directive("sin", function (el, binding) {
+            el.innerHTML = Math.sin(binding.value)
+        });
+        Vue.directive("cos", function (el, binding) {
+            el.innerHTML = Math.cos(binding.value)
+        });
+        Vue.directive("tan", function (el, binding) {
+            el.innerHTML = Math.tan(binding.value)
+        });
+    }
+}
